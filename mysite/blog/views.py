ecfcc276aae09d4959ccb5320bded888ca945cc1
Alexander Levkin <49125444+Comandosss@users.noreply.github.com>
2023-08-14 21:24:43 +0300

    изменил работу функции post_detail для извлечения опубликованного поста с заданным слагом и датой публикации; добавил в функцию post_list постраничную разбивку, импортировав, класс Paginator и обработку исключений PageNotAnInteger и EmptyPage; добавил функцию post_share для отправки постов по email; добавил post_comment для управления передачей поста на обработку и набор запросов QuerySet, чтобы извлекать все активные комментарии к посту; изменил представление post_list, для отображения всех постов, помеченных конкретным тегом; добавил отображение схожих постов по числу имеющихся у них общих тегов в post_detail c помощью функции агрегирования Count; добавил полнотекстовой поиск по триграммному сходству

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,14 +1,101 @@
+import os
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
+from django.contrib.postgres.search import TrigramSimilarity
+from django.db.models import Count
+from taggit.models import Tag
 from .models import Post
+from .forms import EmailPostFrom, CommentForm, SearchForm
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     """Извлечение всех постов c статусом PUBLISHED"""
-    posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+    # Постраничная разбивка с 3 постами на страницу
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', {'posts': posts, 'tag': tag})
 
 
-def post_detail(request, id):
-    """Предоставление детальной информации о посте"""
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
-    return render(request, 'blog/post/detail.html', {'post': post})
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, status=Post.Status.PUBLISHED,
+                             slug=post, publish__year=year,
+                             publish__month=month, publish__day=day)
+    # Список активных комментариев к этому посту
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+    # Список схожих постов
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids) \
+        .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')) \
+        .order_by('-same_tags', '-publish')[:4]
+    return render(request, 'blog/post/detail.html',
+                  {'post': post, 'comments': comments,
+                   'form': form, 'similar_posts': similar_posts})
+
+
+def post_share(request, post_id):
+    # Извлечь пост по идентификатору id
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+    if request.method == 'POST':
+        # Форма была передана на обработку
+        form = EmailPostFrom(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f'{cd["name"]} recommends you read {post.title}'
+            message = (f'Read this article "{post.title}" at {post_url}\n\n'
+                       f'{cd["name"]} ({cd["email"]}) '
+                       f'comments: {cd["comments"]}')
+            send_mail(subject, message, os.getenv('EMAIL_USER'), [cd['to']],
+                      fail_silently=False)
+            sent = True
+    else:
+        form = EmailPostFrom()
+    return render(request, 'blog/post/share.html',
+                  {'post': post, 'form': form, 'sent': sent})
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, 
+                             status=Post.Status.PUBLISHED)
+    comment = None
+    # Комментарий был отправлен
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        # Назначить пост комментарию
+        comment.post = post
+        comment.save()
+    return render(request, 'blog/post/comment.html',
+                  {'post': post, 'form': form, 'comment': comment})
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.published.annotate(
+                similarity=TrigramSimilarity('title', query),
+            ).filter(similarity__gt=0.1).order_by('-similarity')
+    return render(request, 'blog/post/search.html',
+                  {'form': form, 'query': query, 'results': results})
