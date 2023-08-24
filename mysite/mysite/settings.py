60eccecaff1e39db527922620a0ba2f658a99b3d
Alexander Levkin <49125444+Comandosss@users.noreply.github.com>
2023-08-24 12:33:34 +0300

    LOGIN_REDIRECT_URL для отправки пользователя, после входа в лк, на главную страницу; установил SESSION_COOKIE_AGE в 30 дней; добавил MEDIA_ROOT, MEDIA_URL для хранения файлов; добавил в AUTHENTICATION_BACKENDS механизмы аутентификации Google и Github; добавил accounts.apps.AccountsConfig, social_django, django_summernote, django-bootstrap5 в INSTALLED_APPS; добавил SUMMERNOTE_THEME.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -49,6 +49,9 @@ INSTALLED_APPS = [
     'django.contrib.sitemaps',
     'django.contrib.postgres',
     'accounts.apps.AccountsConfig',
+    'social_django',
+    'django_summernote',
+    'django_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -74,6 +77,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect',
             ],
         },
     },
@@ -147,3 +152,23 @@ EMAIL_USE_TLS = True
 LOGIN_REDIRECT_URL = '/'
 
 SESSION_COOKIE_AGE = 60 * 60 * 24 * 30
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+
+# Список серверов аутентификации
+AUTHENTICATION_BACKENDS = (
+    'social_core.backends.github.GithubOAuth2',
+    'social_core.backends.google.GoogleOAuth2',
+    'django.contrib.auth.backends.ModelBackend',
+)
+
+# Social auth configs for github
+SOCIAL_AUTH_GITHUB_KEY = os.getenv('GITHUB_KEY')
+SOCIAL_AUTH_GITHUB_SECRET = os.getenv('GITHUB_SECRET')
+
+# Social auth configs for google
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.getenv('GOOGLE_KEY')
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.getenv('GOOGLE_SECRET')
+
+SUMMERNOTE_THEME = 'bs5'
